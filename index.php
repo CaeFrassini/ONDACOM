a2078dbd3a806c8ab47c539852b954cadd89c98d
CaeFrassin <caetanoxd07@gmail.com>
2026-06-25 16:01:23 -0300

    Alteração no head

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="<?=BASE_URL?>assets/css/style.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
