d5f7dc1904fbd69cd162f4eeaad8145689b485f7
CaeFrassin <caetanoxd07@gmail.com>
2026-07-22 10:04:31 -0300

    Alteração dos pdfs e alteração de caminho

--- pags/gov.php
+++ pags/gov.php
@@ -40,14 +40,14 @@ require_once '../vendor/autoload.php';
         <div class="politica-icon">
             <img src="<?=BASE_URL?>assets/img/politica-privacidade_icon.png" alt="Política de Privacidade"> </div>
         <h3>Política de Privacidade</h3>
-        <a href="<?=BASE_URL?>assets/docs/politica-de-privacidade.pdf" target="_blank" class="btn-acessar-pdf">ACESSAR</a>
+        <a href="<?=BASE_URL?>assets/docs/privacidade.pdf" target="_blank" class="btn-acessar-pdf">ACESSAR</a>
     </div>
 
     <div class="politica-card">
         <div class="politica-icon">
             <img src="<?=BASE_URL?>assets/img/protecao-de-dados_icon.png" alt="Proteção de dados"> </div>
         <h3>Governança e Proteção de Dados</h3>
-        <a href="<?=BASE_URL?>assets/docs/politica-governanca-protecao-dados.pdf" target="_blank" class="btn-acessar-pdf">ACESSAR</a>
+        <a href="<?=BASE_URL?>assets/docs/governanca-protecao-dados.pdf" target="_blank" class="btn-acessar-pdf">ACESSAR</a>
     </div>
 
     <div class="politica-card">
@@ -61,28 +61,28 @@ require_once '../vendor/autoload.php';
         <div class="politica-icon">
             <img src="<?=BASE_URL?>assets/img/codigo-etica_icon.png" alt="Etica e conduta"> </div>
         <h3>Código de Ética e Conduta</h3>
-        <a href="<?=BASE_URL?>assets/docs/codigo-de-etica.pdf" target="_blank" class="btn-acessar-pdf">ACESSAR</a>
+        <a href="<?=BASE_URL?>assets/docs/etica-e-conduta.pdf" target="_blank" class="btn-acessar-pdf">ACESSAR</a>
     </div>
 
     <div class="politica-card">
         <div class="politica-icon">
             <img src="<?=BASE_URL?>assets/img/combate-corrupcao_icon.png" alt="Combate a corrupação"> </div>
         <h3>Combate à Corrupção</h3>
-        <a href="<?=BASE_URL?>assets/docs/combate-a-corrupcao.pdf" target="_blank" class="btn-acessar-pdf">ACESSAR</a>
+        <a href="<?=BASE_URL?>assets/docs/combate-corrupcao.pdf" target="_blank" class="btn-acessar-pdf">ACESSAR</a>
     </div>
 
     <div class="politica-card">
         <div class="politica-icon">
             <img src="<?=BASE_URL?>assets/img/combate-ao-assedio_icon.png" alt="Combate ao assédio"> </div>
         <h3>Combate ao Assédio</h3>
-        <a href="<?=BASE_URL?>assets/docs/combate-ao-assedio.pdf" target="_blank" class="btn-acessar-pdf">ACESSAR</a>
+        <a href="<?=BASE_URL?>assets/docs/combate-assedio.pdf" target="_blank" class="btn-acessar-pdf">ACESSAR</a>
     </div>
 
     <div class="politica-card">
         <div class="politica-icon">
             <img src="<?=BASE_URL?>assets/img/politica-sustentabilidade_icon.png" alt="Política de Sustentabilidade"> </div>
         <h3>Política de Sustentabilidade</h3>
-        <a href="<?=BASE_URL?>assets/docs/politica-sustentabilidade.pdf" target="_blank" class="btn-acessar-pdf">ACESSAR</a>
+        <a href="<?=BASE_URL?>assets/docs/sustentabilidade.pdf" target="_blank" class="btn-acessar-pdf">ACESSAR</a>
     </div>
 
 </div>
